fc99f10d900852422b7fce280f92ff49bc71ddfc
d3Cod3Monk <umeshmarlon@gmail.com>
2021-02-15 18:25:39 +0530

    Course Schema added

--- seeder.js
+++ seeder.js
@@ -6,13 +6,16 @@ import connectDB from './config/db.js'
 dotenv.config();
 
 import Bootcamp from './models/Bootcamp.js';
+import Course from './models/Course.js';
 connectDB();
 
 const bootcamps = JSON.parse(fs.readFileSync(`./_data/bootcamps.json` , 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`./_data/courses.json` , 'utf-8'));
 
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data are fetched to the database'.green.bold.inverse);
         process.exit();
     } catch (error) {
@@ -23,6 +26,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data are deleted'.red.bold.inverse);
         process.exit();
     } catch (error) {
